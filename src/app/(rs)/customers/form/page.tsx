c9bc285fbc44c1da9b8ed2dc7b38253f724a5861
Olga <ms.estete@gmail.com>
2025-08-22 12:23:56 -0500

    feat: add customer and ticket form

--- src/app/(rs)/customers/form/page.tsx
+++ src/app/(rs)/customers/form/page.tsx
@@ -1,6 +1,8 @@
 import { getCustomer } from "@/lib/queries/getCustomer";
 import { BackButton } from "@/components/BackButton";
-import * as Sentry from "@sentry/nextjs"
+import * as Sentry from "@sentry/nextjs";
+
+import CustomerForm from "@/app/(rs)/customers/form/CustomerForm";
 
 export default async function CustomerFormPage({
     searchParams,
@@ -23,9 +25,9 @@ export default async function CustomerFormPage({
                 )
             }
             console.log(customer)
-            // put customer form component 
+            return <CustomerForm customer={customer} />
         } else {
-            // new customer form component 
+            return <CustomerForm/>
         }
 
     } catch (e) {
--- src/app/(rs)/tickets/form/page.tsx
+++ src/app/(rs)/tickets/form/page.tsx
@@ -2,6 +2,7 @@ import { getCustomer } from '@/lib/queries/getCustomer';
 import { getTicket } from '@/lib/queries/getTicket';
 import { BackButton } from '@/components/BackButton';
 import * as Sentry from '@sentry/nextjs';
+import TicketForm from "@/app/(rs)/tickets/form/TicketForm";
 
 export default async function TicketFormPage({
   searchParams,
@@ -44,6 +45,7 @@ export default async function TicketFormPage({
 
       // return ticket form
       console.log(customer);
+      return <TicketForm customer={customer} />
     }
 
     // Edit ticket form
@@ -64,6 +66,7 @@ export default async function TicketFormPage({
       // return ticket form
       console.log('ticket: ', ticket);
       console.log('customer: ', customer);
+      return <TicketForm customer={customer} ticket={ticket} />
     }
   } catch (e) {
     if (e instanceof Error) {
